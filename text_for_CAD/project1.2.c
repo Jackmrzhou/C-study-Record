c55b08708403ec97093f86af82c3597a92302c49
Jack <594780735@qq.com>
2017-03-26 19:46:39 +0800

    finish module

--- text_for_CAD/project1.2.c
+++ text_for_CAD/project1.2.c
@@ -24,16 +24,17 @@
 #define NOT_INSERT FALSE
 #define Start_X 0
 const int ms500 = 500;
-struct CAD_Text{
-    bool is_Chinese = FALSE;
+
+typedef struct CAD_Text{
+    bool is_Chinese;
     unsigned char text[1000];
     //store text
-    unsigned char temp_char[3] = {'\0', '\0', '\0'};
+    unsigned char temp_char[3];
     //store a character which is temporary used
-    unsigned char draw_char[3] = {'\0', '\0', '\0'};
+    unsigned char draw_char[3];
     //store a character which is to be drown
     int record_Chinese_index[500];
-    int Chinese_counter = 0;
+    int Chinese_counter;
     int text_index;
     //text_index also represents cursor location
     int current_line;
@@ -42,7 +43,7 @@ struct CAD_Text{
     //record the width of char which is deleted
     double last_char_X[10];
     //record every line's last char's x location
-};
+}CAD_TEXT;
 
 bool is_display = FALSE;
 //cursor status
@@ -66,6 +67,16 @@ void keyboardEventProcess(int key,int event);
 void TimerEventProcess(int timerID);
 void startTimer(int timerID, int timeinterval);
 
+CAD_TEXT global_text={FALSE,
+            {0},
+            {'\0', '\0', '\0'},
+            {'\0', '\0', '\0'},
+            {0},
+            0,
+            0,
+            0,
+            0,
+            {0}};
 void Main()
 {
     double cy;
@@ -83,9 +94,9 @@ void CharEventProcess(char c)
     if (c == '\r')
     {
         int lenth;
-        lenth = strlen(text);
-        text[lenth] = '\0';
-        puts(text);
+        lenth = strlen(global_text.text);
+        global_text.text[lenth] = '\0';
+        puts(global_text.text);
     }
     else if (c == VK_BACK || c == VK_DELETE || c == VK_SPACE)
         return;
@@ -93,29 +104,29 @@ void CharEventProcess(char c)
         //space is not allowed
     else
 	{   
-        if (text_index == strlen(text))
+        if (global_text.text_index == strlen(global_text.text))
         {
             Draw_one_char(c);
-            text[text_index++] = c;
+            global_text.text[global_text.text_index++] = c;
         }
         else
         {
-            if (!is_Chinese && c < 0)
+            if (!global_text.is_Chinese && c < 0)
             //the prototype of function is (char c) so when Chinese the c is negative
             {
-                is_Chinese = TRUE;
+                global_text.is_Chinese = TRUE;
                 insert_char(c);
             }
-            else if (is_Chinese)
+            else if (global_text.is_Chinese)
             {
-                is_Chinese = FALSE;
-                record_Chinese_index[Chinese_counter++] = text_index + 1;
+                global_text.is_Chinese = FALSE;
+                global_text.record_Chinese_index[global_text.Chinese_counter++] = global_text.text_index + 1;
                 insert_char(c);
-                text_index--;
+                global_text.text_index--;
             }
             else
                 insert_char(c);
-            if (!is_Chinese)
+            if (!global_text.is_Chinese)
                 ReDraw_text(INSERT);
         }
 	}
@@ -128,25 +139,25 @@ void keyboardEventProcess(int key,int event)
             switch(key)
             {
                 case VK_BACK:
-                    if (text_index > 0)
+                    if (global_text.text_index > 0)
                     {
                         Delete_one_char(LEFT);
                         ReDraw_text(NOT_INSERT);
                     }
                     break;
                 case VK_DELETE:
-                    if (text_index < strlen(text))
+                    if (global_text.text_index < strlen(global_text.text))
                     {
                         Delete_one_char(RIGHT);
                         ReDraw_text(NOT_INSERT);
                     }
                     break;
                 case VK_LEFT:
-                    if (text_index > 0)
+                    if (global_text.text_index > 0)
                         cursor_move(LEFT);
                     break;
                 case VK_RIGHT:
-                    if (text_index < strlen(text))
+                    if (global_text.text_index < strlen(global_text.text))
                         cursor_move(RIGHT);
                     break;
             }
@@ -189,25 +200,24 @@ void Erase_cursor()
 
 void Draw_one_char(unsigned char c)
 {
-    if (draw_char[0] == '\0')
-        draw_char[0] = c;
+    if (global_text.draw_char[0] == '\0')
+        global_text.draw_char[0] = c;
     else
-        draw_char[1] = c;
+        global_text.draw_char[1] = c;
     if (is_display)
         Erase_cursor();
-    if (GetCurrentX() + TextStringWidth(draw_char) > GetWindowWidth())
+    if (GetCurrentX() + TextStringWidth(global_text.draw_char) > GetWindowWidth())
     {
-        last_char_X[current_line++] = GetCurrentX();
+        global_text.last_char_X[global_text.current_line++] = GetCurrentX();
         MovePen(Start_X, GetCurrentY() - line_height);
     }
-    if (draw_char[0] <= 127 || draw_char[1] != '\0')
+    if (global_text.draw_char[0] <= 127 || global_text.draw_char[1] != '\0')
     {
         MovePen(GetCurrentX() + TextStringWidth("|"), GetCurrentY());
-        DrawTextString(draw_char);
-        draw_char[0] ='\0';
-        draw_char[1] = '\0';
-    }
-    
+        DrawTextString(global_text.draw_char);
+        global_text.draw_char[0] ='\0';
+        global_text.draw_char[1] = '\0';
+    } 
 }
 
 void Delete_one_char(int is_LEFT)
@@ -218,45 +228,45 @@ void Delete_one_char(int is_LEFT)
     if (is_LEFT)
     {
         get_near_char(is_LEFT);
-        if (strlen(temp_char) == 2)
+        if (strlen(global_text.temp_char) == 2)
         {
-            text[--text_index] = '\0';
-            text[--text_index] = '\0';
-            strcat(text, &text[text_index+2]);
+            global_text.text[--global_text.text_index] = '\0';
+            global_text.text[--global_text.text_index] = '\0';
+            strcat(global_text.text, &global_text.text[global_text.text_index+2]);
             delete_Chinese_index();
         }
         else
         {
-            text[--text_index] = '\0';
-            strcat(text, &text[text_index+1]);
+            global_text.text[--global_text.text_index] = '\0';
+            strcat(global_text.text, &global_text.text[global_text.text_index+1]);
         }
         //cursor move
-        char_width = TextStringWidth(temp_char);
-        if (GetCurrentX() - char_width < Start_X)
-            MovePen(last_char_X[--current_line], GetCurrentY() + line_height);
-        MovePen(GetCurrentX()-char_width,GetCurrentY());
-        DrawTextString(temp_char);
-        MovePen(GetCurrentX()-char_width-TextStringWidth("|"),GetCurrentY());
+        global_text.char_width = TextStringWidth(global_text.temp_char);
+        if (GetCurrentX() - global_text.char_width < Start_X)
+            MovePen(global_text.last_char_X[--global_text.current_line], GetCurrentY() + line_height);
+        MovePen(GetCurrentX()-global_text.char_width,GetCurrentY());
+        DrawTextString(global_text.temp_char);
+        MovePen(GetCurrentX()-global_text.char_width-TextStringWidth("|"),GetCurrentY());
     }
     else
     {
         get_near_char(is_LEFT);
-        if (strlen(temp_char) == 2)
+        if (strlen(global_text.temp_char) == 2)
         {
-            text[text_index] = '\0';
-            text[text_index + 1] = '\0';
-            strcat(text, &text[text_index+2]); 
+            global_text.text[global_text.text_index] = '\0';
+            global_text.text[global_text.text_index + 1] = '\0';
+            strcat(global_text.text, &global_text.text[global_text.text_index+2]); 
         }
         else
         {
-            text[text_index] = '\0';
-            strcat(text, &text[text_index+1]);
+            global_text.text[global_text.text_index] = '\0';
+            strcat(global_text.text, &global_text.text[global_text.text_index+1]);
         }
         //cursor not move
-        char_width = TextStringWidth(temp_char);
+        global_text.char_width = TextStringWidth(global_text.temp_char);
         MovePen(GetCurrentX()+TextStringWidth("|"), GetCurrentY());
-        DrawTextString(temp_char);
-        MovePen(GetCurrentX()-char_width-TextStringWidth("|"),GetCurrentY());
+        DrawTextString(global_text.temp_char);
+        MovePen(GetCurrentX()-global_text.char_width-TextStringWidth("|"),GetCurrentY());
     }
     clear_temp_char();
     SetEraseMode(FALSE);
@@ -268,33 +278,33 @@ void cursor_move(int is_LEFT)
     if (is_LEFT)
     {
         get_near_char(is_LEFT);
-        if (strlen(temp_char) == 2)
-            text_index -= 2;
+        if (strlen(global_text.temp_char) == 2)
+            global_text.text_index -= 2;
         else
-            text_index--;
-        if (GetCurrentX() -TextStringWidth(temp_char) > 0)
-            MovePen(GetCurrentX()-TextStringWidth(temp_char)-TextStringWidth("|"), GetCurrentY());
+            global_text.text_index--;
+        if (GetCurrentX() -TextStringWidth(global_text.temp_char) > 0)
+            MovePen(GetCurrentX()-TextStringWidth(global_text.temp_char)-TextStringWidth("|"), GetCurrentY());
         else
         {
-            MovePen(last_char_X[--current_line], GetCurrentY() + line_height);
-            text_index = (temp_char[1] == '\0')?(text_index + 1):(text_index + 2);
+            MovePen(global_text.last_char_X[--global_text.current_line], GetCurrentY() + line_height);
+            global_text.text_index = (global_text.temp_char[1] == '\0')?(global_text.text_index + 1):(global_text.text_index + 2);
             //reset text_index
         }
 	}
     else
 	{
         get_near_char(is_LEFT);
-        if (strlen(temp_char) == 2)
-            text_index += 2;
+        if (strlen(global_text.temp_char) == 2)
+            global_text.text_index += 2;
         else
-            text_index++;
-        if (GetCurrentX() + TextStringWidth(temp_char) < GetWindowWidth())
-            MovePen(GetCurrentX()+TextStringWidth(temp_char)+TextStringWidth("|"), GetCurrentY());
+            global_text.text_index++;
+        if (GetCurrentX() + TextStringWidth(global_text.temp_char) < GetWindowWidth())
+            MovePen(GetCurrentX()+TextStringWidth(global_text.temp_char)+TextStringWidth("|"), GetCurrentY());
         else
         {
             MovePen(Start_X, GetCurrentY() - line_height);
-            current_line++;
-            text_index = (temp_char[1] == '\0')?(text_index - 1):(text_index - 2);
+            global_text.current_line++;
+            global_text.text_index = (global_text.temp_char[1] == '\0')?(global_text.text_index - 1):(global_text.text_index - 2);
         }
     }
     clear_temp_char();
@@ -304,57 +314,57 @@ void ReDraw_text(int is_insert)
 {
     double original_x,original_y;
     int original_index,original_current_line;
-    original_index = text_index;
+    original_index = global_text.text_index;
     original_x = GetCurrentX();
     original_y = GetCurrentY();
-    original_current_line = current_line;
+    original_current_line = global_text.current_line;
     
     if (is_display)
         Erase_cursor();
     cancelTimer(cursor_500);
     //stop the cursor for a while
     if (!is_insert)
-        MovePen(original_x + char_width + TextStringWidth("|"), original_y);
+        MovePen(original_x + global_text.char_width + TextStringWidth("|"), original_y);
     else
     {
         get_near_char(RIGHT);
-        text_index += strlen(temp_char);
+        global_text.text_index += strlen(global_text.temp_char);
         clear_temp_char();
     }
 
-    while(text_index < strlen(text))
+    while(global_text.text_index < strlen(global_text.text))
     {
         Erase_one_char();
-        text_index++;
+        global_text.text_index++;
     }
     //erase rest string
 
     MovePen(original_x, original_y);
     //move cursor to original palce
-    text_index = original_index;
-    current_line = original_current_line;
+    global_text.text_index = original_index;
+    global_text.current_line = original_current_line;
 
-    while (text_index < strlen(text))
+    while (global_text.text_index < strlen(global_text.text))
     {
         
-        Draw_one_char(text[text_index]);
-        text_index++;
+        Draw_one_char(global_text.text[global_text.text_index]);
+        global_text.text_index++;
     }
     //redraw rest string
 
     if (!is_insert)
     {
-        text_index = original_index;
+        global_text.text_index = original_index;
         MovePen(original_x, original_y);
     }
     else
     {
-        text_index = original_index;
+        global_text.text_index = original_index;
         get_near_char(RIGHT);
-        MovePen(original_x + TextStringWidth("|") + TextStringWidth(temp_char), original_y);
-        text_index += strlen(temp_char);
+        MovePen(original_x + TextStringWidth("|") + TextStringWidth(global_text.temp_char), original_y);
+        global_text.text_index += strlen(global_text.temp_char);
     }
-    current_line = original_current_line;
+    global_text.current_line = original_current_line;
     clear_temp_char();
     startTimer(cursor_500, ms500);
     //restart cursor
@@ -366,69 +376,69 @@ void Erase_one_char()
     cx = GetCurrentX();
     cy = GetCurrentY();
     SetEraseMode(TRUE);
-    Draw_one_char(text[text_index]);
+    Draw_one_char(global_text.text[global_text.text_index]);
     SetEraseMode(FALSE);
 }
 void insert_char(unsigned char c)
 {
     int index;
-    index = strlen(text);
-    text[index + 1] = '\0'; 
-    while(index > text_index)
+    index = strlen(global_text.text);
+    global_text.text[index + 1] = '\0'; 
+    while(index > global_text.text_index)
     {
-        text[index] = text[index - 1];
+        global_text.text[index] = global_text.text[index - 1];
         index--;
     }
-    text[text_index] = c;
-    if (is_Chinese)
-        text_index++;  
+    global_text.text[global_text.text_index] = c;
+    if (global_text.is_Chinese)
+        global_text.text_index++;  
 }
 void get_near_char(int is_LEFT)
 {  
     int loop;
     if (is_LEFT)
     {
-        for (loop = 0;loop < Chinese_counter;loop++)
+        for (loop = 0;loop < global_text.Chinese_counter;loop++)
         {
-            if (text_index == record_Chinese_index[loop])
+            if (global_text.text_index == global_text.record_Chinese_index[loop])
             {
-                temp_char[0] = text[text_index - 2];
-                temp_char[1] = text[text_index - 1];
+                global_text.temp_char[0] = global_text.text[global_text.text_index - 2];
+                global_text.temp_char[1] = global_text.text[global_text.text_index - 1];
                 break;
             }
         }
-        if (strlen(temp_char) != 2)
-            temp_char[0] = text[text_index - 1];
+        if (strlen(global_text.temp_char) != 2)
+            global_text.temp_char[0] = global_text.text[global_text.text_index - 1];
     }
     else
     {
-        if (text[text_index] > 127)
+        if (global_text.text[global_text.text_index] > 127)
         {
-            temp_char[0] = text[text_index];
-            temp_char[1] = text[text_index + 1];
+            global_text.temp_char[0] = global_text.text[global_text.text_index];
+            global_text.temp_char[1] = global_text.text[global_text.text_index + 1];
         }
         else
-            temp_char[0] =text[text_index];
+            global_text.temp_char[0] =global_text.text[global_text.text_index];
     }
 }
 void clear_temp_char()
 {
-    temp_char[0] = '\0';
-    temp_char[1] = '\0';
+    global_text.temp_char[0] = '\0';
+    global_text.temp_char[1] = '\0';
 }
 
 void delete_Chinese_index()
 {
     int index;
-    for (index = 0; index < Chinese_counter; index++)
+    for (index = 0; index < global_text.Chinese_counter; index++)
     {
-        if (record_Chinese_index[index] == text_index + 2)
+        if (global_text.record_Chinese_index[index] == global_text.text_index + 2)
             break;
     }
-    while(index < Chinese_counter)
+    while(index < global_text.Chinese_counter)
     {
-        record_Chinese_index[index] = record_Chinese_index[index + 1];
+        global_text.record_Chinese_index[index] = global_text.record_Chinese_index[index + 1];
         index++;
     }
-    Chinese_counter--;
+    global_text.Chinese_counter--;
 }
