1a65a81ed54944d24792823329059138c252f236
Jack <594780735@qq.com>
2017-03-30 14:59:04 +0800

    init

---
+++ homework2/integer_division.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int result[100];
+int number;
+void division(int number,int index);
+int main(void)
+{
+    int index;
+    scanf("%d",&number);
+    division(number,0);
+    return 0;
+}
+void division(int num,int index)
+{
+    int i;
+    if (num == 0 && index != 1)
+    {
+        printf("%d=",number); 
+        for (i =0; i < index - 1;i++)
+            printf("%d+",result[i]);
+        printf("%d\n", result[i]);
+    }
+    for (i = num;i > 0; i--)
+    {
+        if (index == 0 || i <= result[index - 1])
+        {
+            result[index] = i;
+            division(num - i,index + 1);
+        }
+    }
+}
