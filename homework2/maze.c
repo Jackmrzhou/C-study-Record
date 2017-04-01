644cf5c10af521f4c29ba70caba24cdd2fe2be7f
Jack <594780735@qq.com>
2017-04-01 16:25:49 +0800

    update

--- homework2/maze.c
+++ homework2/maze.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "unsize_array.h"
-#define END_X 7
-#define END_Y 4
+typedef struct STRUCT_MAZE{
+    int map[10][10];
+    int out_X;
+    int out_Y;
+}MAZE;
 int count_path = 0;
 int path[20][50][2];
-struct
-void search_maze(int x,int y,int (*maze)[6],link_array *node);
-void write_into_link(link_array *head);
-void move(int x,int y);
+void search_maze(int x,int y,MAZE *maze,link_array *try_path,link_array *success_path);
+void write_into_link(link_array *success_path,link_array *try_path);
+void move(int x,int y,MAZE *maze,link_array *try_path,link_array *success_path);
 int main(void)
 {
-    int maze[9][6]={
+    MAZE maze;
+    maze->map={
         {0,0,0,0,0,0},
         {0,2,1,1,0,0},
         {0,1,0,1,1,0},
@@ -22,109 +25,78 @@ int main(void)
         {0,1,1,1,1,0},
         {0,0,0,0,0,0}
     };
-    int depth = 0;
-    link_array *array_shortest_head;
-    array_shortest_head = create_link();
-    search_maze(1,1,maze,head); 
-    print_link_array(array_shortest_head);
-    if (count_path == 0)
+    maze->out_X = 7;
+    maze->out_Y = 4;
+    link_array *success_path,*try_path;
+    success_path = create_link();
+    search_maze(1,1,maze,try_path,success_path); 
+    
+    if (success_path->next->data[0] == -1)
         printf("NO PATH!");
+    else
+        print_link_array(success_path);
 }
-void search_maze(int x,int y,int (*maze)[6],link_array *node)
+void search_maze(int x,int y,MAZE *maze,link_array *try_path,link_array *success_path)
 {
     int i;
     for (i = 0;i < 4;i++)
     switch (i)
     {   
         case 0:
-            if (maze[x+1][y] == 1)
-            {
-                move(x+1, y);
-                pre = pre->next;
-                write_into_link(x+1,y,link_array *pre);
-                is_out(x+1, y);
-                if ((x+1) == END_X && y == END_Y)
-                {
-                    maze[x+1][y] = 1;
-                    return;
-                }
-                else
-                {
-                    search_maze(x+1, y,maze,count_step);
-                    maze[x+1][y] = 1;
-                }
-            }
+            move(x+1,y,maze,try_path,success_path);
             break;
         case 1:
-            if (maze[x][y+1] == 1)
-            {
-                maze[x][y+1] = 2;
-                path[count_path][count_step][0] = x;
-                path[count_path][count_step][1] = y+1;
-                count_step++;
-                if (x == END_X && (y+1) == END_Y)
-                {
-                    count_path++;
-                    maze[x][y+1] = 1;
-                    return;
-                }
-                else
-                {
-                    search_maze(x, y+1,maze,count_step);
-                    maze[x][y+1] = 1;
-                }
-            }
+            move(x,y+1,maze,try_path,success_path);
             break;
         case 2:
-            if (maze[x][y-1] == 1)
-            {
-                maze[x][y-1] = 2;
-                path[count_path][count_step][0] = x;
-                path[count_path][count_step][1] = y-1;
-                count_step++;
-                if (x == END_X && (y-1) == END_Y)
-                {
-                    count_path++;
-                    maze[x][y-1] = 1;
-                    return;
-                }
-                else
-                {
-                    search_maze(x, y-1,maze,count_step);
-                    maze[x][y-1] = 1;
-                }
-            }
+            move(x,y-1,maze,try_path,success_path);
             break;
         case 3:
-            if (maze[x-1][y] == 1)
-            {
-                maze[x-1][y] = 2;
-                path[count_path][count_step][0] = x-1;
-                path[count_path][count_step][1] = y;
-                count_step++;
-                if (x-1 == END_X && y == END_Y)
-                {
-                    count_path++;
-                    maze[x-1][y] = 1;
-                    return;
-                }
-                else
-                {
-                    search_maze(x-1, y,maze,count_step);
-                    maze[x-1][y] = 1;
-                }
-            }
+            move(x-1,y,maze,try_path,success_path);
             break;
     }
 }
 
-void move(int x,int y, int (*maze)[6])
-{
-    maze[x][y] = 2;
+void move(int x,int y,MAZE *maze,link_array *try_path,link_array *success_path)
+{ 
+    if (maze[x][y] == 1)
+    {
+        maze[x][y] = 2;
+        move(x, y);
+        try_path = write_into_link(x,y,link_array *try_path);
+        if ((x) == maze->out_X && y == maze->out_Y)
+        {
+            maze[x][y] = 1;
+            copy(link_array *success_path,link_array *try_path);
+            return;
+        }
+        else
+        {
+            search_maze(x, y,maze,try_path, success_path);
+            maze[x][y] = 1;
+        }
+    }
 }
 void write_into_link(int x,int y,link_array *node)
 {
-    node->data[0] = x;
-    node->data[1] = y;
-    node->next = NULL;
+    link_array *next_node;
+    next_node->data[0] = x;
+    next_node->data[1] = y;
+    node->next = next_node;
+    next_node->next = node->next;
+    return next_node;
+}
+
+void copy(link_array *success_path,link_array *try_path)
+{
+    link_array *success_p = success_path;
+    link_array *try_p = try_path;
+    while(try_p->next != try_path)
+    {
+        *success_p = *try_path;
+        success_p->next = success_p;
+        success_p->next = success_path;
+        try_p = try_p->next;
+    }
+    return success_p->next;
 }
--- homework2/unsize_array.c
+++ homework2/unsize_array.c
@@ -7,7 +7,7 @@ typedef struct Array_node{
 link_array *create_link();
 link_array *insert_node(link_array *head,link_array *array);
 void print_link_array(link_array *head);
-void clear_link(link_array *head);
+/*
 int main(void)
 {
     int i;
@@ -23,50 +23,33 @@ int main(void)
     print_link_array(head);
     return 0;
 }
-
+test link_array
+*/
 link_array *create_link()
 {
     link_array *head;
-    head = NULL;
+    head = (link_array*)malloc(sizeof(link_array));
+    head->data[0] = -1;
+    head->data[1] = -1;
+    //init data to show it is head.
     head->next = head;
     return head;
 }
 link_array *insert_node(link_array *head, link_array *array)
 {
-    if (head == NULL)
-    {
-        head = array;
-        head->next = head;
-    }
-    else
-    {
-        link_array *p = head;
-        while(p->next != head)
-            p = p-> next;
-        p->next = array;
-        array->next = head;
-    }
+    link_array *p = head;
+    while (p->next != head)
+        p = p->next;
+    p->next = array;
+    array->next = head;
 }
-void print_link_array(link_array *head)
+void print_path(link_array *head,MAZE *maze)
 {
     link_array *p = head;
     do
     {
-        printf("(%d,%d)",p->data[0],p->data[1]);
         p = p->next;
+        printf("(%d,%d)",p->data[0],p->data[1]);
     }
-    while (p->next != head);
-}
-void clear_link(link_array *head)
-{
-    link_array *p;
-    if (head == NULL)
-        return;
-    while (head != NULL)
-    {
-        p = head->next;
-        free(head);
-        head = p;
-    }
-    head->next = NULL;
+    while (p->next != head && (p->data[0] != maze->out_X && p-> != maze->out_Y));
 }
