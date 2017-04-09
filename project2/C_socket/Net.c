8cff52e2bbecf3df16e16ae032459bf097f1122a
Jack <594780735@qq.com>
2017-04-09 18:59:19 +0800

    upgrade

--- project2/C_socket/Net.c
+++ project2/C_socket/Net.c
@@ -3,12 +3,12 @@
 #include "test.h"
 #define default_port 5000
 #pragma comment (lib, "ws2_32.lib")
-
 char* getIP(); 
 void Start_Server(void);
 void Start_Server(void)
 {
     char *ip;
+    int ret;
     WSADATA wsaData;
     WSAStartup(MAKEWORD(2, 2), &wsaData);
 
@@ -16,21 +16,43 @@ void Start_Server(void)
     ip = "127.0.0.1";
     puts(ip);
 
-    SOCKET ServeSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    struct sockaddr_in Serve_addr;
-    memset(&Serve_addr, 0, sizeof(Serve_addr));
-    Serve_addr.sin_family = AF_INET;
-    Serve_addr.sin_addr.s_addr = inet_addr(ip);
-    Serve_addr.sin_port = htons(default_port);
-    
-    bind(ServeSocket, (SOCKADDR*)&Serve_addr, sizeof(SOCKADDR));
-    listen(ServeSocket, 1);
-    puts("start listening...");
+	SOCKET ServeSocket = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in Serve_addr;
+	memset(&Serve_addr, 0, sizeof(Serve_addr));
+	Serve_addr.sin_family = AF_INET;
+	Serve_addr.sin_addr.s_addr = inet_addr(ip);
+	Serve_addr.sin_port = htons(default_port);
+
+	ret = bind(ServeSocket, (SOCKADDR*)&Serve_addr, sizeof(SOCKADDR));
+    if (ret == SOCKET_ERROR)
+    {
+        printf("Bind Error!");
+        closesocket(ServeSocket);
+        WSACleanup();
+        return;
+    }
+
+	ret = listen(ServeSocket, 1);
+    if (ret == SOCKET_ERROR)
+    {
+        printf("Listen Error!");
+        closesocket(ServeSocket);
+        WSACleanup();
+        return;
+    }
+	printf("start listening...");
 
     struct sockaddr_in ClientAddr;
     int nSize = sizeof(SOCKADDR);
     SOCKET ClientSocket = accept(ServeSocket, (SOCKADDR*)&ClientAddr, &nSize);
-    puts("get connected.");
+    if(ClientSocket == INVALID_SOCKET)
+    {
+        printf("Accept socket Error!");
+        closesocket(ServeSocket);
+        WSACleanup();
+        return;
+    }
+    printf("Accept one connect.")
     while (1)
     {
         while(1)
@@ -47,7 +69,6 @@ void Start_Server(void)
                 {
                     send(ClientSocket, Queue, strlen(Queue), 0);
                     Queue = NULL;
-                    puts("server send");
                     break;
                 }
         }
@@ -55,17 +76,12 @@ void Start_Server(void)
         int ret = recv(ClientSocket, recvData, 1024,0);
         if (ret > 0)
         {
-           strcat(recvData, "Server.");
            Get_Data = recvData;
         } 
     }
 }   
 char* getIP()  
-{  
-    
-    //WSADATA wsaData;
-    //WSAStartup( MAKEWORD(2, 2), &wsaData); // 加载套接字库    
-  
+{    
     int i = 0;  
     struct hostent *phostinfo = gethostbyname("");    
     for(i = 0; NULL != phostinfo&& NULL != phostinfo->h_addr_list[i]; ++i)  
@@ -73,8 +89,6 @@ char* getIP()
         char *pszAddr = inet_ntoa(*(struct in_addr *)phostinfo->h_addr_list[i]);  
         return pszAddr;  
     }  
-  
-    //WSACleanup( );    
 } 
 
 void Start_Client(char * ip);
@@ -82,10 +96,11 @@ void Start_Client(char * ip)
 {
     //char *ip;
     //ip = "127.0.0.1";
+    int ret;
     WSADATA wsaData;
     WSAStartup(MAKEWORD(2,2), &wsaData);
 
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in sock_addr;
     memset(&sock_addr, 0, sizeof(sock_addr));
@@ -93,15 +108,21 @@ void Start_Client(char * ip)
     sock_addr.sin_addr.s_addr = inet_addr(ip);
     sock_addr.sin_port = htons(default_port);
     
-    connect(sock, (SOCKADDR *)&sock_addr, sizeof(SOCKADDR));
-    puts("connect successfully.");
+    ret = connect(sock, (SOCKADDR *)&sock_addr, sizeof(SOCKADDR));
+    if (ret == SOCKET_ERROR)
+    {
+        printf("Connect Error!Please Check IP");
+        closesocket(sock);
+        WSACleanup();
+        return;
+    }
+    printf("Successfully connected!");
     while (1)
     {
         char recvData[1024]={0};
         int ret = recv(sock, recvData, 1024, 0);
         if (ret > 0)
         {
-            strcat(recvData, "Client.");
             Get_Data = recvData;
         }
         while (1)
@@ -117,7 +138,6 @@ void Start_Client(char * ip)
                 {
                     send(sock, Queue, strlen(Queue), 0);
                     Queue = NULL;
-                    puts("client send");
                     break;
                 }
         }
